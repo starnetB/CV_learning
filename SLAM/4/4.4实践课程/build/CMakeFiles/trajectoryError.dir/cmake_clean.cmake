631751ba1da022a85fc7f882abc4a7f6c952a58f
李丹清 <lidanqing@lidanqingdeMacBook-Pro.local>
2021-12-05 20:23:59 +0800

    CV_learning_12_3

---
+++ SLAM/4/4.4实践课程/build/CMakeFiles/trajectoryError.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trajectoryError.dir/src/trajectoryError.cpp.o"
+  "CMakeFiles/trajectoryError.dir/src/trajectoryError.cpp.o.d"
+  "bin/trajectoryError"
+  "bin/trajectoryError.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trajectoryError.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
