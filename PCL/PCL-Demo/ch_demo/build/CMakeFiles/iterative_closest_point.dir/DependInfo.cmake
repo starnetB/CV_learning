8b67bd1aded268eaed4d2f63b366b71386b15756
lees <2102690391@qq.com>
2022-01-05 08:54:36 +0800

    CVL_1_5

---
+++ PCL/PCL-Demo/ch_demo/build/CMakeFiles/iterative_closest_point.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lees/CV_learning/PCL/PCL-Demo/ch_demo/src/3_adad/iterative_closest_point.cpp" "/home/lees/CV_learning/PCL/PCL-Demo/ch_demo/build/CMakeFiles/iterative_closest_point.dir/src/3_adad/iterative_closest_point.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_DATE_TIME_DYN_LINK"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_IOSTREAMS_DYN_LINK"
+  "BOOST_SERIALIZATION_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_WIDGETS_LIB"
+  "qh_QHpointer"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-7.1"
+  "/usr/include/freetype2"
+  "/usr/local/include/pcl-1.12"
+  "/usr/include/eigen3"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PCL/PCL-Demo/ch_demo/build/CMakeFiles/iterative_closest_point.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iterative_closest_point.dir/src/3_adad/iterative_closest_point.cpp.o"
+  "bin/iterative_closest_point"
+  "bin/iterative_closest_point.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iterative_closest_point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
