f35fcf72f875efc8776c104939977c4aeacb6090
ziye01 <2102690391@qq.com>
2022-03-02 19:53:20 +0800

    CVL_3_2

---
+++ SLAM/10_后端2/10_3实践_位姿图优化.md/intro.md
@@ -0,0 +1,7 @@
+# 数据
+往数据里面增加噪声  ，注意，两者之间T_{ij}应该还是没有变化的
+
+
+数据格式     
+VERTEX_SE3:QUAT  x, y, z ,  qx, qy,  qz,   qw
+EDGE_SE3：QUAT  x,y,z,qx,qy,qz,qw  + 6x6的信息矩阵
--- SLAM/9_后端1/9_4实践_g2o求解BA.md/src/bundle_adjustment_g20.cpp
+++ SLAM/9_后端1/9_4实践_g2o求解BA.md/src/bundle_adjustment_g20.cpp
@@ -6,6 +6,7 @@
 #include <g2o/core/robust_kernel_impl.h>
 #include <iostream>
 
+
 #include "common.h"
 #include "sophus/se3.hpp"
 
@@ -14,115 +15,115 @@ using namespace Eigen;
 using namespace std;
 
 /// 姿态和内参的结构
-struct PoseAndIntrinsics {
-    PoseAndIntrinsics() {}
+struct PoseAndIntrinsics{
+    PoseAndIntrinsics(){}
 
     /// set from given data address
-    explicit PoseAndIntrinsics(double *data_addr) {
-        rotation = SO3d::exp(Vector3d(data_addr[0], data_addr[1], data_addr[2]));
-        translation = Vector3d(data_addr[3], data_addr[4], data_addr[5]);
-        focal = data_addr[6];    //焦距
-        k1 = data_addr[7];    //畸变参数
-        k2 = data_addr[8];
+    explicit PoseAndIntrinsics(double *data_addr){
+        rotation = SO3d::exp(Vector3d(data_addr[0], data_addr[1], data_addr[2]));  //旋转向量转旋转矩阵
+        translation=Vector3d(data_addr[3],data_addr[4],data_addr[5]);
+        focal=data_addr[6];
+        k1=data_addr[7];
+        k2=data_addr[8];
     }
 
-    /// 将估计值放入内存
-    void set_to(double *data_addr) {
-        auto r = rotation.log();
-        for (int i = 0; i < 3; ++i) data_addr[i] = r[i];
-        for (int i = 0; i < 3; ++i) data_addr[i + 3] = translation[i];
-        data_addr[6] = focal;
-        data_addr[7] = k1;
-        data_addr[8] = k2;
+    ///将估计放入内存
+    void set_to(double *data_addr){
+        auto r=rotation.log();
+        for(int i=0;i<3;++i) data_addr[i]=r[i];
+        for(int i=0;i<3;++i) data_addr[i+3]=translation[i];
+        data_addr[6]=focal;
+        data_addr[7]=k1;
+        data_addr[8]=k2;
     }
 
     SO3d rotation;
-    Vector3d translation = Vector3d::Zero();
-    double focal = 0;
-    double k1 = 0, k2 = 0;
+    Vector3d translation=Vector3d::Zero();
+
+    double focal=0;
+    double k1=0,k2=0;
 };
 
 /// 位姿加相机内参的顶点，9维，前三维为so3，接下去为t, f, k1, k2
-class VertexPoseAndIntrinsics : public g2o::BaseVertex<9, PoseAndIntrinsics> {
+class VertexPoseAndIntrinsics:public g2o::BaseVertex<9,PoseAndIntrinsics>{
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-    VertexPoseAndIntrinsics() {}
+    VertexPoseAndIntrinsics(){}
 
     virtual void setToOriginImpl() override {
         _estimate = PoseAndIntrinsics();
-    }
+    } //指向点对象
 
-    virtual void oplusImpl(const double *update) override {
-        _estimate.rotation = SO3d::exp(Vector3d(update[0], update[1], update[2])) * _estimate.rotation;
-        _estimate.translation += Vector3d(update[3], update[4], update[5]);
-        _estimate.focal += update[6];
-        _estimate.k1 += update[7];
-        _estimate.k2 += update[8];
+    virtual void opluImpl(const double *update) override{
+        _estimate.rotation=SO3d::exp(Vector3d(update[0],update[1],update[2]))*_estimate.rotation;
+        _estimate.translation+=Vector3d(update[3],update[4],update[5]);
+        _estimate.focal+=update[6];
+        _estimate.k1+=update[7];
+        _estimate.k2+=update[8];
     }
 
     /// 根据估计值投影一个点
-    Vector2d project(const Vector3d &point) {
-        Vector3d pc = _estimate.rotation * point + _estimate.translation;
-        pc = -pc / pc[2];
-        double r2 = pc.squaredNorm();
-        double distortion = 1.0 + r2 * (_estimate.k1 + _estimate.k2 * r2);
-        return Vector2d(_estimate.focal * distortion * pc[0],
-                        _estimate.focal * distortion * pc[1]);
-    }
+    Vector2d project(const Vector3d &point){
+        Vector3d pc=_estimate.rotation*point+_estimate.translation;
+        pc=-pc/pc[2];
+        double r2=pc.squaredNorm();  //计算畸变参数
+        double distortion=1.0+r2*(_estimate.k1+_estimate.k2*r2);
+        return Vector2d(_estimate.focal*distortion*pc[0],
+                        _estimate.focal*distortion*pc[1]);
+    }  //计算像素误差
 
-    virtual bool read(istream &in) {}
+    virtual bool read(istream &in){}
 
     virtual bool write(ostream &out) const {}
 };
 
-class VertexPoint : public g2o::BaseVertex<3, Vector3d> {
+//这个是三维定点 ，相机点
+class VertexPoint:public g2o::BaseVertex<3,Vector3d>{
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-    VertexPoint() {}
+    VertexPoint(){}
 
-    virtual void setToOriginImpl() override {
+    virtual void setToOriginImpl() override{
         _estimate = Vector3d(0, 0, 0);
     }
 
-    virtual void oplusImpl(const double *update) override {
+    virtual void oplusImpl(const double *update) override{
         _estimate += Vector3d(update[0], update[1], update[2]);
     }
 
-    virtual bool read(istream &in) {}
+    virtual bool read(istream &in){}
 
     virtual bool write(ostream &out) const {}
 };
 
-class EdgeProjection :
-    public g2o::BaseBinaryEdge<2, Vector2d, VertexPoseAndIntrinsics, VertexPoint> {
+//第一个数表示残差范围，第二个数残差类型，然后是一个参数块，第二个参数块的维度
+class EdgeProjection:public g2o::BaseBinaryEdge<2,Vector2d,VertexPoseAndIntrinsics,VertexPoint>
+{
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
 
-    virtual void computeError() override {
-        auto v0 = (VertexPoseAndIntrinsics *) _vertices[0];
+    virtual void computeError() override{
+        auto v0=(VertexPoseAndIntrinsics *) _vertices[0];
         auto v1 = (VertexPoint *) _vertices[1];
-        auto proj = v0->project(v1->estimate());
-        _error = proj - _measurement;
+        auto proj=v0->project(v1->estimate());
+        _error=proj-_measurement;
     }
 
-    // use numeric derivatives
-    virtual bool read(istream &in) {}
+    //use numeric derivatives
+    virtual bool read(istream &in){}
 
-    virtual bool write(ostream &out) const {}
-
+    virtual bool write(ostream &out) const{}
 };
 
 void SolveBA(BALProblem &bal_problem);
 
-int main(int argc, char **argv) {
-
+int main(int argc,char **argv){
     if (argc != 2) {
         cout << "usage: bundle_adjustment_g2o bal_data.txt" << endl;
         return 1;
     }
-
     BALProblem bal_problem(argv[1]);
     bal_problem.Normalize();
     bal_problem.Perturb(0.1, 0.5, 0.5);
@@ -133,7 +134,7 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void SolveBA(BALProblem &bal_problem) {
+void SolveBA(BALProblem *bal_problem){
     const int point_block_size = bal_problem.point_block_size();
     const int camera_block_size = bal_problem.camera_block_size();
     double *points = bal_problem.mutable_points();
@@ -142,9 +143,11 @@ void SolveBA(BALProblem &bal_problem) {
     // pose dimension 9, landmark is 3
     typedef g2o::BlockSolver<g2o::BlockSolverTraits<9, 3>> BlockSolverType;
     typedef g2o::LinearSolverCSparse<BlockSolverType::PoseMatrixType> LinearSolverType;
-    // use LM
+
+    //use LM
     auto solver = new g2o::OptimizationAlgorithmLevenberg(
         g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+    
     g2o::SparseOptimizer optimizer;
     optimizer.setAlgorithm(solver);
     optimizer.setVerbose(true);
@@ -152,7 +155,9 @@ void SolveBA(BALProblem &bal_problem) {
     /// build g2o problem
     const double *observations = bal_problem.observations();
     // vertex
+    //构建位置点
     vector<VertexPoseAndIntrinsics *> vertex_pose_intrinsics;
+    //构建像素点的容器
     vector<VertexPoint *> vertex_points;
     for (int i = 0; i < bal_problem.num_cameras(); ++i) {
         VertexPoseAndIntrinsics *v = new VertexPoseAndIntrinsics();
@@ -199,4 +204,5 @@ void SolveBA(BALProblem &bal_problem) {
         auto vertex = vertex_points[i];
         for (int k = 0; k < 3; ++k) point[k] = vertex->estimate()[k];
     }
+
 }
