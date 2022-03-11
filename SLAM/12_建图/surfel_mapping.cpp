6165478d7dc1ccd68bff141c5dba340410fb6cfe
ziye01 <2102690391@qq.com>
2022-03-11 14:18:37 +0800

    CVL_3_11_1

--- SLAM/12_建图/surfel_mapping.cpp
+++ SLAM/12_建图/surfel_mapping.cpp
@@ -34,14 +34,69 @@ SurfelCloudPtr reconstructSurface(const PointCloudPtr &input,
 }
 
 //构建三角网格
-pcl::PolygonMeshPtr triangularMesh(const SurfelCloudPtr &surfels)
+pcl::PolygonMeshPtr triangulateMesh(const SurfelCloudPtr &surfels)
 {
     //Create search tree
     pcl::search::KdTree<SurfelT>::Ptr tree(new pcl::search::KdTree<SurfelT>);
     tree->setInputCloud(surfels);
 
-    //Initial objectes
-    //以下内容请见贪婪投影算法  
+   // Initialize objects
+    pcl::GreedyProjectionTriangulation<SurfelT> gp3;
+    pcl::PolygonMeshPtr triangles(new pcl::PolygonMesh);
+
+    // Set the maximum distance between connected points (maximum edge length)
+    gp3.setSearchRadius(0.05);  //搜索半径的设定（这个参数必须由用户指定），它决定的重建后三角形的大小。
+
+    // Set typical values for the parameters
+    //mu是个加权因子，对于每个参考点，其映射所选球的半径由mu与离参考点最近点的距离乘积所决定，这样就很好解决了点云密度不均匀的问题，mu一般取值为2.5-3。
+    gp3.setMu(2.5);
+    gp3.setMaximumNearestNeighbors(100);  //最大的邻近点数量
+    /*
+    两点的法向量角度差大于此值，
+    这两点将不会连接成三角形，
+    这个就恰恰和点云局部平滑的约束呼应，
+    如果一个点是尖锐点那么它将不会和周围的点组成三角形，
+    其实这个也有效的滤掉了一些离群点。这个值一般为45度。*/
+    gp3.setMaximumSurfaceAngle(M_PI / 4); // 45 degrees
+    gp3.setMinimumAngle(M_PI / 18); // 10 degrees
+    gp3.setMaximumAngle(2 * M_PI / 3); // 120 degrees 三角形最大、最小角度的阈值。
+    /*
+    输入的法向量是否连续变化的。
+    这个一般来讲是false，
+    除非输入的点云是全局光滑的（比如说一个球）。*/
+    gp3.setNormalConsistency(true);
+
+    // Get result
+    gp3.setInputCloud(surfels);
+    gp3.setSearchMethod(tree);
+    gp3.reconstruct(*triangles);
+
+    return triangles;
+}
 
 
+int main(int argc,char **argv)
+{
+    //Load the points
+    PointCloudPtr cloud(new PointCloud);
+    if (argc == 0 || pcl::io::loadPCDFile(argv[1], *cloud)) {
+        cout << "failed to load point cloud!";
+        return 1;
+    }
+    cout<<"Point cloud loaded ,points:   "<<cloud->points.size()<<endl;
+    //Compute surface elements
+    cout << "computing normals ... " << endl;
+    double mls_radius = 0.05, polynomial_order = 2;
+    auto surfels = reconstructSurface(cloud, mls_radius, polynomial_order);
+
+    // Compute a greedy surface triangulation
+    cout << "computing mesh ... " << endl;
+    pcl::PolygonMeshPtr mesh = triangulateMesh(surfels);
+
+    cout << "display mesh ... " << endl;
+    pcl::visualization::PCLVisualizer vis;
+    vis.addPolylineFromPolygonMesh(*mesh, "mesh frame");
+    vis.addPolygonMesh(*mesh, "mesh");
+    vis.resetCamera();
+    vis.spin();
 }
