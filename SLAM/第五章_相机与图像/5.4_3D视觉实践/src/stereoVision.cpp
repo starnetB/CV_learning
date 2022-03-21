0b1a806353f1f2168aecb95d6bda3fe01df59f29
lees <2102690391@qq.com>
2022-03-21 23:08:24 +0800

    cv_3_21_1

--- SLAM/第五章_相机与图像/5.4_3D视觉实践/src/stereoVision.cpp
+++ SLAM/第五章_相机与图像/5.4_3D视觉实践/src/stereoVision.cpp
@@ -22,25 +22,57 @@ void showPointCloud(const vector<Vector4d,Eigen::aligned_allocator<Vector4d>> &p
         cerr<<"Point cloud is empty"<<endl;
         return;
     }
-
+    //create pangolin window and plot the trajectory
     pangolin::CreateWindowAndBind("Point Cloud Viewer",1024,768);
+    /**
+     * glEnable(GL_DEPTH_TEST)：
+     *  用来开启更新深度缓冲区的功能，也就是，如果通过比较后深度值发生变化了，会进行更新深度缓冲区的操作。
+     * 启动它，OpenGL就可以跟踪再Z轴上的像素，这样，它只会再那个像素前方没有东西时，才会绘画这个像素。
+     * 在做绘画3D时，这个功能最好启动，视觉效果比较真实。
+     * */
     glEnable(GL_DEPTH_TEST);
+    /**
+     * Blend 混合是将源色和目标色以某种方式混合生成特效的技术。混合常用来绘制透明或半透明的物体。
+     * 在混合中起关键作用的α值实际上是将源色和目标色按给定比率进行混合，以达到不同程度的透明。
+     * α值为0则完全透明，α值为1则完全不透明。混合操作只能在RGBA模式下进行，颜色索引模式下无法指定α值。
+     * 物体的绘制顺序会影响到OpenGL的混合处理。*/
     glEnable(GL_BLEND);
+    /**
+     * 如果设置了glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+     * 则表示源颜色乘以自身的alpha 值，目标颜色乘以1.0减去源颜色的alpha值，这样一来，
+     * 源颜色的alpha值越大，则产生的新颜色中源颜色所占比例就越大，而目标颜色所占比例则减小。
+     * 这种情况下，我们可以简单的将源颜色的alpha值理解为“不透明度”。这也是混合时最常用的方式。*/
     glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 
+    //定义投影和初始模型视图矩阵
+    /**
+     * ProjectionMatrix()
+     * 设置相机内参，以及能看到的最大最小距离
+     * 如ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000)
+     * 表示相机分辨率，焦距， 相机光心，最小最大距离*/
+    
+    /**ModelViewLookAt()
+     * 设置观看视角，pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0));
+     * 的意思是在世界坐标(0，-0.1，-1.8)出观看坐标原点（0,0,0）并设置Y轴向上*/
+    
     pangolin::OpenGlRenderState s_cam(
         pangolin::ProjectionMatrix(1024, 768, 500, 500, 512, 389, 0.1, 1000),
         pangolin::ModelViewLookAt(0, -0.1, -1.8, 0, 0, 0, 0.0, -1.0, 0.0)
     );
 
     pangolin::View &d_cam = pangolin::CreateDisplay()
+        //SetBounds 边界设置，最后一个是长宽比 -1024.0f/768.0f
+        //前四个参数表示视图在视窗中的位置(这么写是在屏幕中央)
         .SetBounds(0.0,1.0,pangolin::Attach::Pix(175),1.0,-1024.0f/768.0f)
         .SetHandler(new pangolin::Handler3D(s_cam));
 
     while(pangolin::ShouldQuit()==false){
+         //先清空缓冲区
         glClear(GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
 
+         //激活相机
         d_cam.Activate(s_cam);
+        //清理颜色，也就是背景颜色
         glClearColor(1.0f,1.0f,1.0f,1.0f);
 
         glPointSize(2);
@@ -65,14 +97,19 @@ int main(int argc,char **argv){
     //读取图像
     cv::Mat left=cv::imread(left_file,0);
     cv::Mat right=cv::imread(right_file,0);
-    /*
-    1、minDisparity 最小可能的差异值。通常情况下，它是零，但有时整流算法可能会改变图像，所以这个参数需要作相应的调整。
-    2、numDisparities 最大差异减去最小差异。该值总是大于零。在当前的实现中，该参数必须可以被16整除。
-    3、BLOCKSIZE 匹配的块大小。它必须是> = 1的奇数。通常情况下，它应该在3…11的范围内。
+    /*  
+    1、minDisparity 最小视差d。通常情况下，它是零，但有时整流算法可能会改变图像，所以这个参数需要作相应的调整。
+    2、numDisparities 最大视差减去最小视察。该值总是大于零。在当前的实现中，该参数必须可以被16整除。
+    3、BLOCKSIZE 匹配的块大小。它必须是> = 1的奇数。通常情况下，它应该在3…11的范围内，如何用周边的数来更新代价值。
     4、P1 控制视差平滑度的第一个参数。见下文。
-    5、P2 第二个参数控制视差平滑度。值越大，差异越平滑。P1是相邻像素之间的视差变化加或减1的惩罚。P2是相邻像素之间的视差变化超过1的惩罚。该算法需要P2> P1。请参见stereo_match.cpp示例，其中显示了一些相当好的P1和P2值（分别为8 * number_of_image_channels * SADWindowSize * SADWindowSize和32 * number_of_image_channels * SADWindowSize * SADWindowSize）。
-    6、disp12MaxDiff 左右视差检查中允许的最大差异（以整数像素为单位）。将其设置为非正值以禁用检查。 也就是允许d的最小值
-    7、preFilterCap 预滤波图像像素的截断值。该算法首先计算每个像素的x导数，并通过[preFilterCap，preFilterCap]间隔剪切其值。结果值传递给Birchfield-Tomasi像素成本函数。
+    5、P2 第二个参数控制视差平滑度。值越大，差异越平滑。
+         P1是相邻像素之间的视差变化加或减1的惩罚。
+         P2是相邻像素之间的视差变化超过1的惩罚。该算法需要P2> P1。
+         请参见stereo_match.cpp示例，
+         其中显示了一些相当好的P1和P2值
+        （ 分别为8 * number_of_image_channels * SADWindowSize * SADWindowSize和32 * number_of_image_channels * SADWindowSize * SADWindowSize）。
+    6、disp12MaxDiff 左右视差检查中允许的最大差异（以整数像素为单位）。将其设置为非正值以禁用检查。 也就是允许d的最小值，也就是左右验证中的阈值
+    7、preFilterCap 预滤波图像像素的截断值。该算法首先计算每个像素的x导数，并通过[preFilterCap，preFilterCap]间隔剪切其值。结果值传递给Birchfield-Tomasi像素代价函数。
     8、uniquenessRatio 最佳（最小）计算成本函数值应该“赢”第二个最佳值以考虑找到的匹配正确的百分比保证金。通常，5-15范围内的值就足够了。
     9、speckleWindowSize 平滑视差区域的最大尺寸，以考虑其噪声斑点和无效。将其设置为0可禁用斑点过滤。否则，将其设置在50-200的范围内。
     10、speckleRange 每个连接组件内的最大视差变化。如果你做斑点过滤，将参数设置为正值，它将被隐式乘以16.通常，1或2就足够好了。
@@ -89,7 +126,7 @@ int main(int argc,char **argv){
     rtype 指定从原矩阵进行转换后的数据类型，即目标矩阵m的数据类型。当然，矩阵m的通道数应该与原矩阵一样的。如果rtype是负数，那么m矩阵的数据类型应该与原矩阵一样。
     alpha 缩放因子。默认值是1。即把原矩阵中的每一个元素都乘以alpha。
     beta 增量。默认值是0。即把原矩阵中的每一个元素都乘以alpha，再加上beta。*/
-    //我的理解 输出图像在0-1536 之间，但是从上函数StereoSGBM::create 我们需要0-96.0之间的差异值，因此缩放1.0/16.0的比例
+    //我的理解 输出图像在0-1536 之间，但是从上函数StereoSGBM::create 我们需要0-96.0之间的视差值，因此缩放1.0/16.0的比例
     disparity_sgbm.convertTo(disparity,CV_32F,1.0/16.0f);
 
 
---
+++ SLAM/第五章_相机与图像/5.4_3D视觉实践/立体匹配简介SGM.md
@@ -0,0 +1,64 @@
+﻿# 引言
+这两天在尝试使用opencv的cv::StereoSGBM::create()的api，但很多地方无法理解，因此专门花时间学习了立体匹配的相关内容。看了很多博客和文献终于有了拨开云雾见天日的感觉。这里做一点的总结，当然这里只是介绍了opencv的cv::StereoSGBM::create()的相关算法内容。
+
+## 代价函数
+双目相机存在两幅相似的图片，分别是一左一右，如下图所示![极限搜索示意图](https://img-blog.csdnimg.cn/c6b0d6ffbabd498caa6f45401f8646dc.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)上图来源于[博客](https://blog.csdn.net/He3he3he/article/details/101162766)        
+
+
+火柴的初心,上面详细介绍了立体匹配算法的相关内容，非常感谢博主。        
+对于左图中每一个像素点$I_l(x,y)$，在右图中我们都可以找到对应的像素点$I_r(x,y)$,但实际上左右图是存在一定的视差的，我们将这个视差称为d，那么如果将视差计算进去的话，右图对应的像素应该就是$I_r(x-d,y)$,现在我们取$d\in[0,1,\cdots,15]$的话，我们对应没一个像素就存在一个对应的$I_r(x-d,y)$。
+
+##### AD代价函数
+这里先从AD代价函数开始了解
+$$c(x,y,d)=|I_l(x,y)-I_r(x-d,y)$$
+我们可以看到，对应与没一个像素点/d都存在一个C(p,d)值与之对因，但实际上d是一个数组，有多个值，于是上面代价函数可以使用以下方块表示。     
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/908ce4226afd4a27a3db4855e6aa749e.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_14,color_FFFFFF,t_70,g_se,x_16#pic_center)      
+
+
+这里x,y代表像素点的位置，d表示视差，每个小方格代表一个代价值。我们目标有两个，一个就是要优化这个代价空间，另外一个就是要从代价空间中获取每个(x,y)的正确视差，也就是代价值最小的d。
+
+##### BT代价函数
+BT的代价也是像素灰度值差值的绝对值，不同之处在于BT利用了亚像素的灰度信息。也是使用了插值。这里参考了[博客](https://blog.csdn.net/qq_27606639/article/details/108831965)
+![在这里插入图片描述](https://img-blog.csdnimg.cn/a140cac210c74b14a1a7223659c0fffc.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)     
+
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/660d27df19744b70b0d33d5f86a64431.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)     
+
+## 代价聚合     
+Box Filtering这个其实就是均值滤波      
+![在这里插入图片描述](https://img-blog.csdnimg.cn/5844406490e849bcbc574792ff12d19e.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)     
+
+双边滤波  
+![在这里插入图片描述](https://img-blog.csdnimg.cn/e3a9a3a7b9454f2a970339dfa7b5a401.png?x-oss-process=image/watermark, type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)   
+
+下面是全局的能量函数滤波，这个的话，需要在对一同一d在不同的视差下进行滤波，但是由于时间复杂度太高，因此一般不再实际中使用。     
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/37dd8653ba0e4970a5e0142bca07a1cb.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_17,color_FFFFFF,t_70,g_se,x_16#pic_center)         
+
+由于基于能量函数的话时间复杂度比较高，因此我们往往采用线扫描方式进行优化。  每次优化的时候我们从边上进行动态规划，记录上一次的值，并对每一层d对应代价函数进行优化。      
+
+![在这里插入图片描述](https://img-blog.csdnimg.cn/dadbf3f717d9428f92c79abeca147525.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)      
+ 
+## WTA
+这个简单，就是对于每个像素点上，找到对应的代价值最小的点作为我们需要的视差值。     
+![在这里插入图片描述](https://img-blog.csdnimg.cn/d7f636ced8464fe6849a9391d8a27cb8.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)        
+
+
+## 最后是左右匹配问题   
+![在这里插入图片描述](https://img-blog.csdnimg.cn/38406ef19710450f9de15daf9e25a7b4.png?x-oss-process=image/watermark,type_d3F5LXplbmhlaQ,shadow_50,text_Q1NETiBAd2VpeGluXzM4NDk4NjI5,size_18,color_FFFFFF,t_70,g_se,x_16#pic_center)         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
